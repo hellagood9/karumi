532055407c700a810c0238d2a5ec3f21758494ae
Maximiliano Sussini <maxsus60@gmail.com>
2020-07-24 00:45:19 +0200

    Fixing typo

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -8,7 +8,7 @@ import styles from "./styles.module.scss";
 
 const LoginForm = ({ onSubmit }) => {
   const initialValues = {
-    username: "doe@test.com",
+    username: "",
     password: "",
   };
 
