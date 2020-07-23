48c42554657d2fe7a2d0db18329d9c487c6c96ef
Maximiliano Sussini <maxsus60@gmail.com>
2020-07-23 15:58:04 +0200

    Jest / Enzyme dependencies config and script

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+// import React from "react";
+// import App from "./App";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("adds 1 + 2 to equal 3", () => {
+  function sum(a, b) {
+    return a + b;
+  }
+  expect(sum(1, 2)).toBe(3);
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,14 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import fetchMock from "jest-fetch-mock";
+import Adapter from "enzyme-adapter-react-16";
+require("jest-localstorage-mock");
+
+Enzyme.configure({ adapter: new Adapter() });
+fetchMock.enableMocks();
+
+// Make Enzyme functions available in all test files
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
